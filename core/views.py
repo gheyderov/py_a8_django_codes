8635dd34d7d78d281d9b031cec94ca9fffd69c23
Gunduz Heydarov <g.heyderov@gmail.com>
2024-06-12 19:19:00 +0400

    views.py

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact2.html')
 
 def home(request):
     return render(request, 'index.html')
