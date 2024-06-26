2779b756815be0ba96655dbdac9e50872e190cf3
Gunduz Heydarov <g.heyderov@gmail.com>
2024-06-26 20:24:56 +0400

    django backup

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from products.models import Category, Tag, Recipe
+from products.models import Category, Tag, Recipe, ProductType, Property, PropertyValue
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Recipe)
+admin.site.register(PropertyValue)
+admin.site.register(Property)
+admin.site.register(ProductType)
--- products/models.py
+++ products/models.py
@@ -30,6 +30,9 @@ class Tag(AbstractModel):
     
 
 class Recipe(AbstractModel):
+    product_type = models.ForeignKey('ProductType', related_name='recipes', null=True, blank=True, on_delete=models.CASCADE)
+    values = models.ManyToManyField('PropertyValue', related_name='recipes')
+
     title = models.CharField('title', max_length=255)
     cover_image = models.ImageField('cover_image', upload_to='products/')
     small_description = models.CharField('small_description', max_length=255)
@@ -38,5 +41,31 @@ class Recipe(AbstractModel):
     tags = models.ManyToManyField('Tag', related_name='recipes')
     author = models.ForeignKey(User, related_name='recipes', on_delete=models.CASCADE)
     
+
+
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class ProductType(AbstractModel):
+    title = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class Property(AbstractModel):
+    product_type = models.ForeignKey(ProductType, related_name='properties', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class PropertyValue(AbstractModel):
+    property_id = models.ForeignKey(Property, related_name='values', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
     def __str__(self) -> str:
         return self.title
